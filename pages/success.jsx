dae65760d271adcb62bbacfeb1b12df190b14ebe
Jesus Rondon <jesusf.rondon1809@gmail.com>
2022-01-24 12:51:00 -0500

    success page

--- pages/success.jsx
+++ pages/success.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Header from "../components/Header";
+
+import { CheckCircleIcon } from "@heroicons/react/solid";
+import Link from "next/link";
 
 function success() {
-  return <div>suceed page</div>;
+  return (
+    <div className="h-screen bg-gray-100">
+      <Header />
+
+      <main className="p-0 lg:p-4">
+        <div className="bg-white p-4 md:p-8">
+          <div className="flex items-center space-x-4">
+            <CheckCircleIcon className="h-10 w-10 text-green-600" />
+            <span className="text-xl sz400:text-2xl font-semibold">
+              Thank you, your order has been confirmed!
+            </span>
+          </div>
+
+          <div className="mt-8">
+            <p className="font-medium text-sm sz400:text-base">
+              Thank you for shopping with us. We'll send a confirmation once
+              your item has shipped. If you like to check the status of your
+              order(s), please press the link below
+            </p>
+          </div>
+
+          <Link href="/">
+            <button className="block w-full mt-12 px-4 justify-self-end py-2 font-semibold bg-gradient-to-b from-yellow-200 to-yellow-400">
+              Go to the home page
+            </button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default success;
